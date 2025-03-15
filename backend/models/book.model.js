9ad7fd9214f414ef8ac6aaff53d723e0ebedca43
bugkwuynx <hngo_2028@depauw.edu>
2025-03-15 12:15:24 -0400

    remove code

--- backend/models/book.model.js
+++ backend/models/book.model.js
@@ -1,25 +1,9 @@
 import mongoose from "mongoose";
 
-const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    cover_url: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1535905496755-26ae35d0ae54?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    status: {
-        type: String,
-        enum: ['To Read', 'Reading', 'Finished'],
-        default: 'To Read'
-    }
-}, {timestamps: true});
+// set up schema
 
-const Book = mongoose.model('Book', bookSchema);
+
+// create model
+
 
 export default Book;
--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/books", bookRoutes);
 
-mongoose.connect(process.env.MONGO_URI);
+// Connect to MongoDB
+
+// Connect to routes
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
