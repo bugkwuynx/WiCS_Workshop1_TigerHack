de5b88352d7effda6b7d60018fe14024032dd845
bugkwuynx <hngo_2028@depauw.edu>
2025-03-22 13:14:17 -0400

    add code

--- frontend/src/components/displayBook.jsx
+++ frontend/src/components/displayBook.jsx
@@ -5,7 +5,32 @@ import {Card, CardContent, Typography, CardActions, Button} from "@mui/material"
 const DisplayBook = () => {
 
     // fetch books
-    
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_PORT}/api/books`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error("Failed to fetch books");
+                }
+                const result = await response.json();
+                const res = Array.isArray(result.data) ? result.data : [result.data];
+                setBooks(res);
+                return true;
+            } catch (error) {
+                console.error("Error fetching books:", error);
+                return false;
+            }
+        }
+        fetchBooks();
+    }, []);
+
 
     // handleCreate
 
@@ -17,7 +42,24 @@ const DisplayBook = () => {
             {/* display books */}
             <div style={{ margin: "20px", maxWidth: "800px", width: "100%", marginLeft: "auto", marginRight: "auto" }}>
                 {/* Display books */}
-                
+                {books.map((book, index) => (
+                    <Card key={book._id || index}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                            {book.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {book.author}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {book.status}
+                        </Typography>
+
+
+                        <img src={book.cover_url} width={100} height={100}/>
+                    </CardContent>
+                  </Card>
+                ))}
             </div>
         </>
     );
